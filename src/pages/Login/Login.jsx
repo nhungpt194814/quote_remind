c6ff21cf2b896be4dba827fc629e85149648bec3
nhungpt194814 <nhung.pt194814@gmail.com>
2023-10-29 11:08:06 +0700

    Login finished

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Login.css';
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-export default Login
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLogin = () => {
+    };
+
+    return (
+        <div className='flexColCenter innerWidth login-container'>
+            <h2>Login</h2>
+            <div>
+                <label>Email:</label>
+                <input type='text' value={email} onChange={handleEmailChange} />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type='password' value={password} onChange={handlePasswordChange} />
+            </div>
+            <button className='button' onClick={handleLogin}>Login</button>
+        </div>
+    );
+};
+
+export default Login;
